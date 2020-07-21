ce2376d091b672a785e11f8c7a0f2e8aaa78083d
Andrew MacGinitie <macginitie@charter.net>
2020-07-21 14:11:06 -0400

    removed unnecessary division

--- conway/conway.py
+++ conway/conway.py
@@ -11,7 +11,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import matplotlib.animation as animation
 
-ON = 255
+ON = 1
 OFF = 0
 vals = [ON, OFF]
 
@@ -21,36 +21,36 @@ def randomGrid(N):
 
 def addGlider(i, j, grid):
     """adds a glider with top left cell at (i, j)"""
-    glider = np.array([[0,    0, 255], 
-                       [255,  0, 255], 
-                       [0,  255, 255]])
+    glider = np.array([[0,    0, 1], 
+                       [1,  0, 1], 
+                       [0,  1, 1]])
     grid[i:i+3, j:j+3] = glider
 
 def addGosperGliderGun(i, j, grid):
     """adds a Gosper Glider Gun with top left cell at (i, j)"""
     gun = np.zeros(11*38).reshape(11, 38)
 
-    gun[5][1] = gun[5][2] = 255
-    gun[6][1] = gun[6][2] = 255
+    gun[5][1] = gun[5][2] = 1
+    gun[6][1] = gun[6][2] = 1
 
-    gun[3][13] = gun[3][14] = 255
-    gun[4][12] = gun[4][16] = 255
-    gun[5][11] = gun[5][17] = 255
-    gun[6][11] = gun[6][15] = gun[6][17] = gun[6][18] = 255
-    gun[7][11] = gun[7][17] = 255
-    gun[8][12] = gun[8][16] = 255
-    gun[9][13] = gun[9][14] = 255
+    gun[3][13] = gun[3][14] = 1
+    gun[4][12] = gun[4][16] = 1
+    gun[5][11] = gun[5][17] = 1
+    gun[6][11] = gun[6][15] = gun[6][17] = gun[6][18] = 1
+    gun[7][11] = gun[7][17] = 1
+    gun[8][12] = gun[8][16] = 1
+    gun[9][13] = gun[9][14] = 1
 
-    gun[1][25] = 255
-    gun[2][23] = gun[2][25] = 255
-    gun[3][21] = gun[3][22] = 255
-    gun[4][21] = gun[4][22] = 255
-    gun[5][21] = gun[5][22] = 255
-    gun[6][23] = gun[6][25] = 255
-    gun[7][25] = 255
+    gun[1][25] = 1
+    gun[2][23] = gun[2][25] = 1
+    gun[3][21] = gun[3][22] = 1
+    gun[4][21] = gun[4][22] = 1
+    gun[5][21] = gun[5][22] = 1
+    gun[6][23] = gun[6][25] = 1
+    gun[7][25] = 1
 
-    gun[3][35] = gun[3][36] = 255
-    gun[4][35] = gun[4][36] = 255
+    gun[3][35] = gun[3][36] = 1
+    gun[4][35] = gun[4][36] = 1
 
     grid[i:i+11, j:j+38] = gun
 
@@ -63,10 +63,10 @@ def update(frameNum, img, grid, N):
             # compute 8-neghbor sum
             # using toroidal boundary conditions - x and y wrap around 
             # so that the simulaton takes place on a toroidal surface.
-            total = int((grid[i, (j-1)%N] + grid[i, (j+1)%N] + 
-                         grid[(i-1)%N, j] + grid[(i+1)%N, j] + 
-                         grid[(i-1)%N, (j-1)%N] + grid[(i-1)%N, (j+1)%N] + 
-                         grid[(i+1)%N, (j-1)%N] + grid[(i+1)%N, (j+1)%N])/255)
+            total = (grid[i, (j-1)%N] + grid[i, (j+1)%N] + 
+                     grid[(i-1)%N, j] + grid[(i+1)%N, j] + 
+                     grid[(i-1)%N, (j-1)%N] + grid[(i-1)%N, (j+1)%N] + 
+                     grid[(i+1)%N, (j-1)%N] + grid[(i+1)%N, (j+1)%N])
             # apply Conway's rules
             if grid[i, j]  == ON:
                 if (total < 2) or (total > 3):
